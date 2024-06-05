ef0cf62e9509881979ee7f1fcf7032c78448218d
amiyagiri <amiyakrushnagiri430@gmail.com>
2024-06-06 00:25:13 +0530

    package re-arranged

---
+++ src/main/java/com/banking/dto/AccountDTO.java
@@ -0,0 +1,14 @@
+package com.banking.entity.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class AccountDTO {
+
+	private Long id;
+private String accountHolderName;
+private double balance;
+private Long accountHolderPhoneNo;
+private long accountHolderPinCode;
+}
--- src/main/java/com/banking/entity/dto/AccountDTO.java
+++
@@ -1,5 +0,0 @@
-package com.banking.entity.dto;
-
-public class AccountDTO {
-
-}
